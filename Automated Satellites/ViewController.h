cb486c5dd52dc2a304191dbfe64fda35d44f7f0d
Bridget Johnson <johnsonbridget.d@gmail.com>
2014-06-12 12:33:43 +1200

    Initial Commit

---
+++ Automated Satellites/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Automated Satellites
+//
+//  Created by Bridget Johnson on 12/06/14.
+//  Copyright (c) 2014 bdj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Automated Satellites/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Automated Satellites
+//
+//  Created by Bridget Johnson on 12/06/14.
+//  Copyright (c) 2014 bdj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+   @autoreleasepool {
+       return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+   }
+}
